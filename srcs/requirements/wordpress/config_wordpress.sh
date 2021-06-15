6f5ca4e131a74eacb66ace62bb9328488b33389f
melperri <melperri@student.42.fr>
2021-06-15 19:21:33 +0200

    add modif

--- srcs/requirements/wordpress/config_wordpress.sh
+++ srcs/requirements/wordpress/config_wordpress.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-/etc/init.d/mysql start
-
-sleep 2
-
 echo "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;" | mariadb -u root
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'melperri'@'localhost' IDENTIFIED BY 'password';" | mariadb -u root
 mariadb wordpress < /home/srcs/wordpress.sql -u root
@@ -15,5 +11,3 @@ mariadb < /var/www/my_server.localhost/phpmyadmin/sql/create_tables.sql -u root
 echo "FLUSH PRIVILEGES;" | mariadb -u root
 
 service php7.3-fpm start
-
-nginx -g 'daemon off;'
