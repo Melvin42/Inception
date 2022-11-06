3ff2e025e999c22165685a5fcec46e47cc1e6f31
melperri <melperri@student.42.fr>
2022-11-06 21:31:32 +0100

    ca marche

--- srcs/requirements/mariadb/tools/config_mariadb.sh
+++ srcs/requirements/mariadb/tools/config_mariadb.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-userdel mysql
-useradd -u 999 mysql
-mkdir -p /data/mysql
-
 /etc/init.d/mysql start
 
-sleep 2
-
-echo "CREATE USER 'melperri'@'%' IDENTIFIED BY 'password';" | mariadb -u root
+echo "CREATE USER IF NOT EXISTS 'melperri'@'%' IDENTIFIED BY 'password';" | mariadb -u root
 
 echo "GRANT ALL ON *.* TO 'melperri'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES; " | mariadb -u root
 
 echo "CREATE DATABASE IF NOT EXISTS db; GRANT ALL ON db.* TO 'melperri'@'%' IDENTIFIED BY 'password'; FLUSH PRIVILEGES;" | mariadb -u root
+
+exec "$@"
--- srcs/requirements/wordpress/tools/config_wordpress.sh
+++ srcs/requirements/wordpress/tools/config_wordpress.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cp /var/www/melperri.42.fr/wordpress/wp-config-sample.php /var/www/melperri.42.fr/wordpress/wp-config.php
+cp /var/www/melperri.42.fr/wp-config-sample.php /var/www/melperri.42.fr/wp-config.php
 
-sed 's/database_name_here/$WORDPRESS_DB_NAME/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
-sed 's/username_here/$WORDPRESS_DB_USER/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
-sed 's/password_here/$WORDPRESS_DB_PASSWORD/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
-sed 's/localhost/$WORDPRESS_DB_HOST/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
+sed "s/database_name_here/$WORDPRESS_DB_NAME/" -i /var/www/melperri.42.fr/wp-config.php
+sed "s/username_here/$WORDPRESS_DB_USER/" -i /var/www/melperri.42.fr/wp-config.php
+sed "s/password_here/$WORDPRESS_DB_PASSWORD/" -i /var/www/melperri.42.fr/wp-config.php
+sed "s/localhost/$WORDPRESS_DB_HOST/" -i /var/www/melperri.42.fr/wp-config.php
 
 /etc/init.d/php7.3-fpm start
 /etc/init.d/php7.3-fpm stop
