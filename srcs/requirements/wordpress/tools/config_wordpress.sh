fb547975116b8aef5d491a5d922d89cca6153bd7
melperri <melperri@student.42.fr>
2022-11-06 19:02:03 +0100

    nginx ok

--- srcs/requirements/wordpress/tools/config_wordpress.sh
+++ srcs/requirements/wordpress/tools/config_wordpress.sh
@@ -6,3 +6,8 @@ sed 's/database_name_here/$WORDPRESS_DB_NAME/' -i /var/www/melperri.42.fr/wordpr
 sed 's/username_here/$WORDPRESS_DB_USER/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
 sed 's/password_here/$WORDPRESS_DB_PASSWORD/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
 sed 's/localhost/$WORDPRESS_DB_HOST/' -i /var/www/melperri.42.fr/wordpress/wp-config.php
+
+/etc/init.d/php7.3-fpm start
+/etc/init.d/php7.3-fpm stop
+
+exec "$@"
